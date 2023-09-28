60d57ed02dce73c409d250425cad8263a194dd73
YGVKN <denisyagovkin@mail.ru>
2023-09-28 23:33:39 +0300

    VIM + Clojure Socket Server & more

---
+++ clj-socket/src/clj_socket/core.clj
@@ -0,0 +1,33 @@
+(ns clj-socket.core
+  (:require [clojure.java.io :as io])
+  (:import [java.net ServerSocket])
+  (:gen-class))
+
+
+(defn receive
+  [socket]
+  (.readLine (io/reader socket)))
+
+(defn ssend
+  [socket msg]
+  (let [writer (io/writer socket)]
+      (.write writer msg)
+      (.flush writer)))
+
+(defn serve-persistent [port handler]
+  (let [running (atom true)]
+    (future
+      (with-open [server-sock (ServerSocket. port)]
+        (while @running
+          (with-open [sock (.accept server-sock)]
+            (let [msg-in (receive sock)
+                  msg-out (handler msg-in)]
+              (ssend sock msg-out))))))
+    running))
+
+
+(defn -main [& _]
+  (println "======== Socket Open ========")
+  (serve-persistent 8765 #(.toUpperCase %)))
+
+
